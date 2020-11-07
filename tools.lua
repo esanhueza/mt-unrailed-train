24f6fd6f235f244e6276f07efb7405d4c69b7191
Erick Sanhueza <esanhueza@zohomail.com>
2020-11-07 03:15:22 -0300

    axe item overriden

--- tools.lua
+++ tools.lua
@@ -17,3 +17,23 @@ minetest.override_item("default:pick_steel", {
     return itemstack
   end
 })
+
+minetest.override_item("default:axe_steel", {
+	description = "Steel Axe",
+	inventory_image = "default_tool_steelaxe.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			choppy={times={[1]=2.50, [2]=1.40, [3]=1.00}, uses=20, maxlevel=2},
+		},
+		damage_groups = {fleshy=4},
+	},
+	sound = {breaks = "default_tool_breaks"},
+	groups = {axe = 1},
+  
+  on_place = function(itemstack, placer, pointed_thing)
+    itemstack, pos = minetest.item_place_object(itemstack, placer, pointed_thing)
+    return itemstack
+  end
+})
