d11aab992dc95ec0cd73299e5fe4da137d636791
Erick Sanhueza <esanhueza@zohomail.com>
2020-11-07 03:02:47 -0300

    pick item overriden
    pick can now be placed with right click

---
+++ tools.lua
@@ -0,0 +1,19 @@
+minetest.override_item("default:pick_steel", {
+	description = "Steel Pickaxe",
+	inventory_image = "default_tool_steelpick.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			cracky = {times={[1]=4.00, [2]=1.60, [3]=0.80}, uses=20, maxlevel=2},
+		},
+		damage_groups = {fleshy=4},
+	},
+	sound = {breaks = "default_tool_breaks"},
+	groups = {pickaxe = 1},
+  
+  on_place = function(itemstack, placer, pointed_thing)
+    itemstack, pos = minetest.item_place_object(itemstack, placer, pointed_thing)
+    return itemstack
+  end
+})
